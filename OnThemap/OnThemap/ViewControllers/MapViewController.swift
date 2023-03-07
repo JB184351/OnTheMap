f566a2cadb2bf5d027e409be86be2a3880d7ce96
JB184351 <justinbengtson@icloud.com>
2023-03-07 11:42:16 -0700

    Adding Add and Refresh buttons to MapViewController

--- OnThemap/OnThemap/ViewControllers/MapViewController.swift
+++ OnThemap/OnThemap/ViewControllers/MapViewController.swift
@@ -19,18 +19,38 @@ class MapViewController: UIViewController {
     
     private func setupUI() {
         view.addSubview(mapView)
+        navigationController?.navigationBar.isTranslucent = false
+        navigationItem.backButtonTitle = NSLocalizedString("Logout", comment: "Button's logs the user out of the Udacity website")
+        
         setupConstraints()
+        
+        setupNavBar()
     }
     
     private func setupConstraints() {
         mapView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            mapView.topAnchor.constraint(equalTo: view.topAnchor, constant: view.frame.height / 8),
-            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -view.frame.height / 8),
+            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -view.frame.height / 10),
             mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
+    }
+    
+    private func setupNavBar() {
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
+        let refreshButton = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(refreshButtonTapped))
+        
+        navigationItem.rightBarButtonItems = [addButton, refreshButton]
+    }
+    
+    @objc private func addButtonTapped() {
+        
+    }
+    
+    @objc private func refreshButtonTapped() {
+        
     }
 
 }
